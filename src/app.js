9ab8d675d73e51d559923f925388ae053238afcb
Camille Chabaro <cchabaro@Camilles-MacBook-Pro.local>
2019-12-29 10:29:50 +0700

    Added About Text

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
